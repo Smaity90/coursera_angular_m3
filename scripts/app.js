a9af9982167b0dcecd30a9e01a64fb52d120f117
maitysudipta90 <maitysudipta90@gmail.com>
2016-10-03 20:47:06 +0530

    complted

---
+++ scripts/app.js
@@ -0,0 +1,93 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('itemsLoaderIndicator',ItemLoaderIndicator)
+.directive('foundItems',FoundItemsDerective);
+
+// Controller 
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var ctrl=this;
+  
+  ctrl.narrowItDown=function(){
+    ctrl.foundItems=null;
+     ctrl.errorMsg=null;
+     if(ctrl.searchTerm ==""){
+       ctrl.errorMsg="Nothing Found";
+     }else{
+      var foundItems=MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+      foundItems.then(function(result){
+          console.log("Found Items:",result);
+          if(result.length<1){
+            ctrl.errorMsg="Nothing Found";
+          }else{
+            ctrl.foundItems=result;
+          }
+      });
+     }
+  };
+  ctrl.removeFoundItem=function(index){
+    ctrl.foundItems=MenuSearchService.removeFoundItem(index);
+  }
+  
+}
+
+//directive
+
+function ItemLoaderIndicator(){
+  var ddo={
+    templateUrl:'../loader/itemsloaderindicator.template.html'
+  };
+  return ddo;
+
+}
+function FoundItemsDerective(){
+  var ddo={
+     templateUrl:'../template/itemfound.template.html',
+     scope:{
+       itemList:'<',
+       errorMsg:'@',
+       removeItem:'&removeFoundItem'
+     }
+  };
+  return ddo;
+}
+
+// service
+MenuSearchService.$inject=["$http","ApiBasePath"]
+function MenuSearchService($http,ApiBasePath){
+  var service =this;
+   var foundItems=[];
+
+  service.getMatchedMenuItems=function(searchTerm){
+     return  $http({
+          url:(ApiBasePath+"/menu_items.json"),
+          method:"GET"
+      }).then(function(result){
+          foundItems=[];
+          var menu_items=result.data.menu_items;
+         
+          for(var i=0;i<menu_items.length;i++){
+            var item=menu_items[i];
+            if(item.description.indexOf(searchTerm)!=-1){
+              foundItems.push(item);
+            }
+          }
+          return foundItems;
+      },function(error){
+          console.log("Error:"+error.data);
+      });
+  };// end of getMatchedMenuItems
+
+  service.removeFoundItem=function(index){
+     foundItems.splice(index,1);
+    return foundItems;
+  };
+
+}
+
+})();
